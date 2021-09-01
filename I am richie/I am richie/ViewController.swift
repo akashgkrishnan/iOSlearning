5c6ce1197264014b091f95f949640d5b6e25c7ee
Akash G Krishnan <krishnanag1996@gmail.com>
2021-09-01 23:43:49 +0530

    Add files via upload

---
+++ I am richie/I am richie/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  I am richie
+//
+//  Created by Akash G Krishnan on 01/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
